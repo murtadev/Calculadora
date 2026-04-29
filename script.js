115644723d7048a682c08dc02b5a687aabe1ff2e
murtadev <gustavomurta1313@gmail.com>
2026-04-29 17:22:32 -0300

    funções adicionadas aos botões

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ function execution(numero){
     let visor = document.getElementById("visor");
     visor.value += numero;
 }
+
+function limpar(){
+    let visor = document.getElementById("visor");
+    visor.value = "";
+}
+
+function incremento(numero){
+    let visor = document.getelementById("visor");
+    visor.value = '++',numero;
+}
+
+function subtração(numero){
+    let visor = document.getElementById("visor");
+    visor.value = "-",numero;
+}
+
+function divisão(numero){
+    let visor = document.getElementById("visor");
+    visor.value = "/",numero;
+}
+
+function mulitplicação(numero){
+    let visor = document.getElementById("visor");
+    visor.value = "*",numero;
+}
