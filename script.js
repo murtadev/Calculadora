2c3321a3551d85807d887b16406dcaf3e303fd99
murtadev <gustavomurta1313@gmail.com>
2026-04-30 19:03:11 -0300

    Botão de apagar um caractece por vez adicionado

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ function calcular(){
     let visor = document.getElementById("visor");
     visor.value = eval(visor.value);
 }
+
+function apagar(){
+    let visor = document.getElementById("visor");
+    visor.value = visor.value.slice(0,-1);
+}
